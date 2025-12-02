fc9a4e73e0ce72164694d7a9c44eb828e09495e0
坂井莉久 <sakairiku@Momo-2.local>
2025-12-02 12:39:12 +0900

    Fix 404 on navigation

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/aiu_cedar_society' : '',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
