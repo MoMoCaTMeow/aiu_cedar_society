c4f9f766335eb4dcfaf58b7deaa64a67787c533b
坂井莉久 <sakairiku@Momo-2.local>
2025-12-08 14:22:23 +0900

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,38 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aiu-cedar-society.vercel.app',
+  site: 'https://momo1105.com',
   integrations: [sitemap()],
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+    domains: ['images.microcms-assets.io'],
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: '**.microcms-assets.io',
+    }],
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: false,
+      minify: 'esbuild',
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
+      },
+    },
+    ssr: {
+      noExternal: [],
+    },
+  },
 });
